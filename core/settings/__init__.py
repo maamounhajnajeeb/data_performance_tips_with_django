21b7db62b4794e86ac4473aa18a7a7348316d851
Maamoun Haj Najeeb <maamoun.haj.najeeb@gmail.com>
2024-12-29 11:32:35 +0300

    after adding django-split-settings package

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,4 +1,4 @@
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- general/utils/settings.py
+++ general/utils/settings.py
@@ -5,4 +5,4 @@ from .misc import yaml_coerce
 
 def get_settings_from_env(prefix):
     prefix_len = len(prefix)
-    return {key[prefix_len:]: value for key, value in os.environ.items() if key.startswith(prefix)}
+    return {key[prefix_len:]: yaml_coerce(value) for key, value in os.environ.items() if key.startswith(prefix)}
